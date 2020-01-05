b9af16ade93d6c0610d0954c330f309f7853592a
wellinton1 <skyred1@gmail.com>
2020-01-05 20:43:28 +0000

    albus: use privapp permissions whitelist

--- device.mk
+++ device.mk
@@ -24,6 +24,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+# Qualcomm
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions.xml:/system/etc/permissions/privapp-permissions.xml
+
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
